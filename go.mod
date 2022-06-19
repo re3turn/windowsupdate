612ad3af564e000d4a69d72b72ca043c9b238343
re3turn <re3turn@gmail.com>
2022-06-19 14:38:54 +0900

    Add okToErr
    Signed-off-by: re3turn <re3turn@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/re3turn/windowsupdate
 
 go 1.15
 
-require github.com/go-ole/go-ole v1.2.6
+require (
+	github.com/go-ole/go-ole v1.2.6
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- oleconv.go
+++ oleconv.go
@@ -14,10 +14,17 @@ limitations under the License.
 package windowsupdate
 
 import (
-	"time"
-
 	"github.com/go-ole/go-ole"
+	"github.com/pkg/errors"
+	"time"
 )
+
+func okToErr(ok bool, t string) error {
+	if !ok {
+		return errors.Errorf("Not a %s", t)
+	}
+	return nil
+}
 
 func toIDispatchErr(result *ole.VARIANT, err error) (*ole.IDispatch, error) {
 	if err != nil {
@@ -41,104 +48,116 @@ func toInt64Err(result *ole.VARIANT, err error) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
-	return variantToInt64(result), nil
+	return variantToInt64(result)
 }
 
 func toInt32Err(result *ole.VARIANT, err error) (int32, error) {
 	if err != nil {
 		return 0, err
 	}
-	return variantToInt32(result), nil
+	return variantToInt32(result)
 }
 
 func toFloat64Err(result *ole.VARIANT, err error) (float64, error) {
 	if err != nil {
 		return 0, err
 	}
-	return variantToFloat64(result), nil
+	return variantToFloat64(result)
 }
 
 func toFloat32Err(result *ole.VARIANT, err error) (float32, error) {
 	if err != nil {
 		return 0, err
 	}
-	return variantToFloat32(result), nil
+	return variantToFloat32(result)
 }
 
 func toStringErr(result *ole.VARIANT, err error) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return variantToString(result), nil
+	return variantToString(result)
 }
 
 func toBoolErr(result *ole.VARIANT, err error) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	return variantToBool(result), nil
+	return variantToBool(result)
 }
 
 func toTimeErr(result *ole.VARIANT, err error) (*time.Time, error) {
 	if err != nil {
 		return nil, err
 	}
-	return variantToTime(result), nil
+	return variantToTime(result)
 }
 
-func variantToInt64(v *ole.VARIANT) int64 {
-	value := v.Value()
-	if value == nil {
-		return 0
+func variantToInt64(v *ole.VARIANT) (int64, error) {
+	valueRaw := v.Value()
+	if valueRaw == nil {
+		return 0, nil
 	}
-	return value.(int64)
+
+	value, ok := valueRaw.(int64)
+	return value, okToErr(ok, "int64")
 }
 
-func variantToInt32(v *ole.VARIANT) int32 {
-	value := v.Value()
-	if value == nil {
-		return 0
+func variantToInt32(v *ole.VARIANT) (int32, error) {
+	valueRaw := v.Value()
+	if valueRaw == nil {
+		return 0, nil
 	}
-	return value.(int32)
+	value, ok := valueRaw.(int32)
+	return value, okToErr(ok, "int32")
 }
 
-func variantToFloat64(v *ole.VARIANT) float64 {
-	value := v.Value()
-	if value == nil {
-		return 0
+func variantToFloat64(v *ole.VARIANT) (float64, error) {
+	valueRaw := v.Value()
+	if valueRaw == nil {
+		return 0, nil
 	}
-	return value.(float64)
+
+	value, ok := valueRaw.(float64)
+	return value, okToErr(ok, "float64")
 }
 
-func variantToFloat32(v *ole.VARIANT) float32 {
-	value := v.Value()
-	if value == nil {
-		return 0
+func variantToFloat32(v *ole.VARIANT) (float32, error) {
+	valueRaw := v.Value()
+	if valueRaw == nil {
+		return 0, nil
 	}
-	return value.(float32)
+
+	value, ok := valueRaw.(float32)
+	return value, okToErr(ok, "float32")
 }
 
-func variantToString(v *ole.VARIANT) string {
-	value := v.Value()
-	if value == nil {
-		return ""
+func variantToString(v *ole.VARIANT) (string, error) {
+	valueRaw := v.Value()
+	if valueRaw == nil {
+		return "", nil
 	}
-	return value.(string)
+
+	value, ok := valueRaw.(string)
+	return value, okToErr(ok, "string")
 }
 
-func variantToBool(v *ole.VARIANT) bool {
-	value := v.Value()
-	if value == nil {
-		return false
+func variantToBool(v *ole.VARIANT) (bool, error) {
+	valueRaw := v.Value()
+	if valueRaw == nil {
+		return false, nil
 	}
-	return value.(bool)
+
+	value, ok := valueRaw.(bool)
+	return value, okToErr(ok, "bool")
 }
 
-func variantToTime(v *ole.VARIANT) *time.Time {
-	value := v.Value()
-	if value == nil {
-		return nil
+func variantToTime(v *ole.VARIANT) (*time.Time, error) {
+	valueRaw := v.Value()
+	if valueRaw == nil {
+		return nil, nil
 	}
-	valueTime := value.(time.Time)
-	return &valueTime
+
+	value, ok := valueRaw.(time.Time)
+	return &value, okToErr(ok, "time")
 }
